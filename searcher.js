e7cb31eb6a5e13d3ce234ebf2418f8859c8958f2
Foad Mozaffari <mozafff@modahealth.com>
2014-10-22 14:13:53 -0700

    instagram searcher

--- searcher.js
+++ searcher.js
@@ -2,14 +2,13 @@
  * Created by mozafff on 10/21/2014.
  */
 angular.module('searcher-app',[])
-    .controller('mainCtrl',function($scope,$http,$q ){
+    .controller('mainCtrl',function($scope,$http,$timeout,$q ){
         $scope.submit = function(){
             $scope.makeRequest();
         }
         $scope.makeRequest = function(){
-            var defer = $q.defer();
-
             var url = "https://api.instagram.com/v1/tags/" + $scope.search_term + "/media/recent";
+            console.log(url);
             var request = {
                 client_id : "c6d2780a20b9447d8608b33614424a40",
                 callback: 'JSON_CALLBACK'
@@ -19,11 +18,15 @@ angular.module('searcher-app',[])
                 url: url,
                 params: request
             }).
-                success(function(result) {
-                    $scope.analysis = result;
+                success(function(response) {
+                    $scope.loading = false;
+                    $scope.result = response.data;
                 }).
                 error(function() {
                     alert('error');
                 })
         };
+        $scope.waiting = function(){
+            $scope.loading = true;
+        }
     });
